f79399663ee494bb25f967f59e0c1991adac8055
meatloaf111 <meatloaf111jp@gmail.com>
2018-11-11 10:07:05 +0900

    Fix: imageUrlForRestarurant
    Change the URL to adapt to github pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -153,7 +153,10 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
+    /**
     return (`/img/${restaurant.photograph}`);
+     */
+    return (`mws-restaurant-stage-1/img/${restaurant.photograph}`);
   }
 
   /**
