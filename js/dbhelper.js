46591e1d4d903bbe185c7f5c713f0c0948ef681a
meatloaf111 <meatloaf111jp@gmail.com>
2018-11-11 22:40:22 +0900

    Fix: Path

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,10 +9,12 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    /**
+    
      return `http://localhost:${port}/data/restaurants.json`;
-     */
+    
+    /**
     return `https://meatloaf111.github.io/mws-restaurant-stage-1/data/restaurants.json`;
+    */
   }
 
   /**
