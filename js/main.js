97de56349d85b33925c9c6ec50972d538399cdec
meatloaf111 <meatloaf111jp@gmail.com>
2018-11-12 09:00:03 +0900

    Fix: Accessibility
    Add space between fixed string value and variable

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = "Image of the restaurant" + restaurant.name;
+  image.alt = "Image of the restaurant " + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
