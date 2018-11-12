047fbf1e56bf9ec385a1752f02dc7a4a7b13e7e1
meatloaf111 <meatloaf111jp@gmail.com>
2018-11-13 06:25:44 +0900

    Fix: Service Worker
    Fix fetch function

--- sw.js
+++ sw.js
@@ -52,7 +52,10 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
+            if(response){
+                return response;
+            }
+            return fetch(event.request);
         })
     );
 });
